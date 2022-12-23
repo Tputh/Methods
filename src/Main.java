19c6e42a2a1f98b38784b284dc36dcfc178a028c
Имя <vitya.klyucherov@mail.ru>
2022-12-23 14:38:09 +0300

    Исправления. Дз. Методы

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,60 @@
+import java.lang.reflect.Array;
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.ArrayList;
+
 
 public class Main {
 
 
     // Задача 1
-    public static void leapYear(int issueCount) {
-        System.out.println(issueCount + " год - является високосным");
+    public static void year(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год - является високосным");
+        } else {
+            System.out.println(year + " год - не является високосным");
+        }
     }
-
-    public static void notAleapYear(int issueCount) {
-        System.out.println(issueCount + " год - не является високосным");
-    }
-
-
     // Задача 2
-    public static void yearOfManufactureAndroid(int issueCount) {
-        System.out.println(issueCount + " год. Установите облегченную версию приложения для Android по ссылке!!!");
-    }
 
-    public static void yearOfManufactureIos(int issueCount) {
-        System.out.println(issueCount + " год. Установите облегченную версию приложения для IOS по ссылке!!!");
-    }
+    public static void mobile(int os, int year) {
+        int stop = 2015;
+        if (os < 0 || os > 1) {
+            System.out.println("НЕИЗВЕСТНОЕ УСТРОЙСТВО!!!");
+        }
 
-    // Задача 3
-    public static void daysForDelivery(int issueCount) {
-        System.out.println("Потребуется дней " + issueCount);
+        if (os == 0) {
+            if (year > stop) {
+                System.out.println("Устройство Ios Установите текущую версию");
+            } else {
+                System.out.println("Устройство Ios Установите предыдущую версию");
+            }
+        }
+        if (os == 1) {
+            if (year > stop) {
+                System.out.println("Устройство Android Установите текущую версию");
+            } else {
+                System.out.println("Устройство Android Установите предыдущую версию");
+            }
+        }
+    }
+    public static int deliveryDays(int distance){
+        int deliveryDays = 0;
+        if (distance <= 20){
+            deliveryDays = 1;
+        } else if (distance <= 60) {
+            deliveryDays = 2;
+        } else if (distance <= 100) {
+            deliveryDays = 3;
+            System.out.println("Потребуется " + deliveryDays + " дня для доставки");
+        } else if (distance > 100){
+            System.out.println("Доставки не будет");
+        }
+        return deliveryDays;
     }
 
 
     public static void main(String[] args) {
-        System.out.println("Дз. Методы");
-
+        System.out.println("Исправления. Дз. Методы");
         task1();
         task2();
         task3();
@@ -41,41 +64,30 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-
-        int[] year = {2021};
+        int[] year = {2020, 2022, 2024};
         for (int i = 0; i < year.length; i++) {
-            if ((i + 1) % 4 == 0) {
-
-                leapYear(year[i]);
-            } else {
-                notAleapYear(year[i]);
-            }
+            year(year[i]);
         }
     }
 
+
     public static void task2() {
         System.out.println("Задача 2");
-
-        int[] android = {2012, 2013, 2015, 2016,};
-        int[] ios = {2014, 2015, 2016,};
-        yearOfManufactureAndroid(android[1]);
-        yearOfManufactureIos(ios[0]);
+        mobile(0, 2016);
+        mobile(1, 1);
     }
 
     public static void task3() {
-
-
         System.out.println("Задача 3");
 
-        int deliveryDistance = 95;
-        int deliveryWithin20Km = 1;
-        int deliveryWithin20KmTo60Km = deliveryWithin20Km + 1;
-        int deliveryWithin60KmTo100Km = deliveryWithin20KmTo60Km + 1;
-        boolean numberOfDays = ((deliveryWithin20Km <= 95) && (deliveryWithin20KmTo60Km <= 95) || (deliveryWithin60KmTo100Km <= 95));
-        daysForDelivery(deliveryWithin60KmTo100Km);
+        int distance = 101;
+        deliveryDays(distance);
 
     }
 }
+
+
+
 
 
 
